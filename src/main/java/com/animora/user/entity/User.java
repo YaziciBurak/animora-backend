d49c793f13794a253a162f903af614768ec3646a
burak <burak2880@gmail.com>
2026-01-05 21:28:09 +0300

    feat: add comment orchestration for anime and user

--- src/main/java/com/animora/user/entity/User.java
+++ src/main/java/com/animora/user/entity/User.java
@@ -22,7 +22,7 @@ public class User {
     private Long id;
 
     @Column(nullable = false, unique = true)
-    private String username;
+    private String userName;
 
     @Column(nullable = false, unique = true)
     private String email;
